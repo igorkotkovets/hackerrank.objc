1db2762c23bc859a3d877c3542ab98aa41eece9a
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2020-07-02 09:33:47 +0300

    master Solved Birthday Cake Candles

---
+++ HackerRankObjC/Algorithms/Warmup/Birthday Cake Candles /BirthdayCakeCandles.h
@@ -0,0 +1,17 @@
+//
+//  BirthdayCakeCandles.h
+//  HackerRankObjC
+//
+//  Created by Igor Kotkovets on 7/2/20.
+//  Copyright © 2020 Igor Kotkovets. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BirthdayCakeCandles : NSObject
+- (NSNumber *) birthdayCakeCandles:(NSArray *)arr;
+@end
+
+NS_ASSUME_NONNULL_END
