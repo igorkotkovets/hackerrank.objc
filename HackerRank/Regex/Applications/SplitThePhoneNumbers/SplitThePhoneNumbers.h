c67587f3788c4093242aea70a1553dbadf8fe3bd
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2020-11-08 16:22:44 +0300

    master Solved 'Split the Phone Numbers' https://www.hackerrank.com/challenges/split-number/problem?h_r=next-challenge&h_v=zen

---
+++ HackerRank/Regex/Applications/SplitThePhoneNumbers/SplitThePhoneNumbers.h
@@ -0,0 +1,17 @@
+//
+//  SplitThePhoneNumbers.h
+//  HackerRank
+//
+//  Created by Igor Kotkovets on 11/8/20.
+//  Copyright © 2020 Igor Kotkovets. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SplitThePhoneNumbers : NSObject
+- (NSString *)solveWithInputString:(NSString *)inputString;
+@end
+
+NS_ASSUME_NONNULL_END
--- HackerRank/main.m
+++ HackerRank/main.m
@@ -1,30 +1,28 @@
 
 #import <Foundation/Foundation.h>
+#import "SplitThePhoneNumbers.h"
 
 
 
 int main(int argc, const char* argv[]) {
     @autoreleasepool {
-//        NSData *availableInputData = [[NSFileHandle fileHandleWithStandardInput] availableData];
-//        NSString *availableInputString = [[NSString alloc] initWithData:availableInputData encoding:NSUTF8StringEncoding];
-//        NSFileHandle *stdoutFileHandle = [NSFileHandle fileHandleWithStandardOutput];
-//
-//        NSMutableArray<LanguageDetection *> *detectors = @[
-//            [LanguageDetection CLanguageDetection],
-//            [LanguageDetection JavaLanguageDetection],
-//            [LanguageDetection PythonLanguageDetection]];
-//
-//
-//        for (NSInteger i=0; i<detectors.count; i++) {
-//            LanguageDetection *detector = detectors[i];
-//            NSString *languageOrNil = [detector solveWithInputString:availableInputString];
-//            if (languageOrNil) {
-//                [stdoutFileHandle writeData:[languageOrNil dataUsingEncoding:NSUTF8StringEncoding]];
-//            }
-//        }
-//
-//
-//        [stdoutFileHandle closeFile];
+        NSData *availableInputData = [[NSFileHandle fileHandleWithStandardInput] availableData];
+        NSString *availableInputString = [[NSString alloc] initWithData:availableInputData encoding:NSUTF8StringEncoding];
+        NSArray *availableInputArray = [availableInputString componentsSeparatedByString:@"\n"];
+        SplitThePhoneNumbers *solution = [[SplitThePhoneNumbers alloc] init];
+
+        NSMutableData *outData = [[NSMutableData alloc] initWithCapacity:availableInputData.length];
+        for (NSUInteger i=0; i<availableInputArray.count; i++) {
+            NSString *found = [solution solveWithInputString:availableInputArray[i]];
+            if (found.length) {
+                NSString *withNewLine = [found stringByAppendingString:@"\n"];
+                [outData appendData:[withNewLine dataUsingEncoding:NSUTF8StringEncoding]];
+            }
+        }
+
+        NSFileHandle *stdoutFileHandle = [NSFileHandle fileHandleWithStandardOutput];
+        [stdoutFileHandle writeData:outData];
+        [stdoutFileHandle closeFile];
     }
 
     return 0;
